0b0a73092678c328690a0e363620394ee631b641
huxyierEu88 <kuangpe1992626@126.com>
2021-06-02 17:24:54 +0800

    更名字

--- src/main/java/com/sf/wc/controller/ChineseWordCountController.java
+++ src/main/java/com/sf/wc/controller/ChineseWordCountController.java
@@ -1,8 +1,6 @@
 package com.sf.wc.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,21 +25,11 @@ public class ChineseWordCountController {
 	@RequestMapping("/index")
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView();
-		
-		String text = "海纳百川，有容乃大；壁立千仞，无欲则刚...";
-		Map<Character,Integer> counter = new HashMap<>();
-		for(int i = 0; i < text.length(); i++){
-			char ch = text.charAt(i);
-			if(ch < 0x4E00 || ch > 0x9FA5)
-				continue;
-			counter.put(ch, counter.get(ch) == null ? 0 : counter.get(ch) + 1);			
-		}
-		
-		List<Map.Entry<Character,Integer>> list = new ArrayList<>(counter.entrySet());
-		list.sort((o1,o2) -> o1.getValue() - o2.getValue());
-		list.subList(0, 9);		
-		
-		mv.addObject("list", list);
+		Map<String,Integer> wordMap = new HashMap<>();
+		wordMap.put("你", 10);
+		wordMap.put("好", 20);
+		mv.addObject("wordMap", "wordMap");
+		mv.setViewName("index");
 		return mv;
 	}
 }
